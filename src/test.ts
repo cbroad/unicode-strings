2e007cca314775d1f85d91161176c93027e12af7
Carl Broadbent <cbroadbent@fileshadow.com>
2021-03-10 14:30:27 -0600

    removed srs/test.ts

--- src/test.ts
+++
@@ -1,7 +0,0 @@
-import * as UnicodeStrings from "./UnicodeStrings"
-
-const str = "Hi\nI'm\t칼"
-
-console.log( str );
-console.log( UnicodeStrings.escape( str ) );
-console.log( UnicodeStrings.unescape( UnicodeStrings.escape( str ) ) );
